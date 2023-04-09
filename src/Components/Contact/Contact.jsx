f0fc4bbafd903a85526d44cb4faa979f1ec6c488
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-04-09 16:52:34 +0530

    form added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,23 +1,62 @@
-import React from 'react'
-import "./Contact.css"
+import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <section className='p-[10rem] bg-gray-300 flex justify-between align-center item-center'>
-        <div className=''>
-            <img src="contact.jpg" alt="" className='h-96 w-96 rounded shadow-md' />
-        </div>
-        <div className='  p-6 shadow-lg rounded-xl'>
-            <div className='flex p-4 justify-center items-center text-7xl '><h1 >Contact me</h1></div>
-           
-            <p className='para p-2 overflow-hidden '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ratione quibusdam in velit<br/> sit tempore maiores! Numquam quod impedit magni, fuga quia optio facilis minima? </p>
-            <p className='para p-2  overflow-hidden '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ratione quibusdam in velit<br/> sit tempore maiores! Numquam quod impedit magni, fuga quia optio facilis minima? </p>
-            <div className='pt-10 flex justify-center  items-center '>
-                <button className='w-56 bg-gray-700 text-white text-xl p-5 rounded-md'>Contact</button>
-            </div>
-        </div>
+    <section className="p-[10rem] bg-gray-300 gap-20 ">
+      <div className="flex justify-center item-center text-5xl pb-8 text-gray-500  font-bold "><h1>Contact</h1></div>
+      <div className="flex gap-10">
+        
+        
+          <img
+            src="contact.jpg"
+            alt=""
+            className="h-96 w-96 rounded shadow-md"
+          />
+        
+        
+          <div className="border-2 p-[2rem] h-full w-full">
+            <form action="">
+              <label htmlFor="name">Name:</label>
+              <br />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="rounded-md text-4xl "
+              />
+              <br />
+              <label htmlFor="email">Email:</label>
+              <br />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="rounded-md text-4xl"
+              />
+              <br />
+
+              <label htmlFor="textarea">Message:</label>
+              <br />
+              <textarea
+                name="textarea"
+                id=""
+                cols="20"
+                rows="5"
+                className="w-full h-auto"
+              ></textarea>
+
+              <input
+                type="button"
+                value="submit"
+                className="border-2 p-2 rounded-md bg-green-500 w-full"
+              />
+            </form>
+          </div>
+        
+      </div>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -8,7 +8,10 @@ function LandingPage() {
             <h1 className='pb-8 text-6xl text-blue-200 honetag '>Hello This is Pritam Dhar</h1>
             <h3 className='pb-8 text-3xl text-orange-300 hthree'>I am a Frontend Developer.</h3>
             <p className='pb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Reiciendis sit quam porro autem repudiandae natus!</p>
-            <button className=' border-orange-500 shadow-md p-4 bg-yellow-400 rounded-lg hover:bg-orange-300 hover:-translate-y-4 transition-all'>click me</button>
+         <div className='flex gap-8'>
+         <button className=' border-orange-500 shadow-md p-4 bg-yellow-400 rounded-lg hover:bg-orange-300 hover:-translate-y-4 transition-all'>click me</button>
+            <button className='pl-4 border-orange-500 shadow-md p-4 bg-green-400 rounded-lg hover:bg-orange-300 hover:-translate-y-4 transition-all'>click me</button>
+         </div>
         </div>
 
         <div className=' h-96 bg-yellow-300  rounded-full'>
